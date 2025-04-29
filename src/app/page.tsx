54c97b8d59bffdff386ad7595c8dd7d8d0c1778e
PEDRO HENRIQUE BONOMO SANTOS <1051392411015@fatec>
2025-04-29 11:45:33 -0300

    Em desenvolvimento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Sidebar from "./components/Sidebar/sidebar"
 export default function Home() {
   return (
     <>
-      <main className="bg-[#E8E6E6]">
+      <main className="bg-[#E8E6E]">
         <Sidebar />
       </main>
     </>
